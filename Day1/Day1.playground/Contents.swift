42da53b619604bce5127b21bd16e6050ca7095bc
rolisanchez <roli.s91@gmail.com>
2020-01-07 00:09:11 +0800

    Day1

---
+++ Day1/Day1.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+// Variables
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+// Strings and Integers
+
+var age = 38
+var population = 8_000_000
+
+// Multi-line strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+
+var pi = 3.141
+
+var awesome = true
+
+// String interpolation
+
+var score = 85
+var stri = "Your score was \(score)"
+
+var results = "The test results are here: \(stri)"
+
+// Constants
+
+let taylor = "swift"
+
+// Type annotations
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+// Simple types: Summary
