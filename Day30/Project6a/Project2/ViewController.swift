027ee78a423d4d8f14894170701dd08e385e7c5e
rolisanchez <roli.s91@gmail.com>
2020-02-03 14:37:06 +0800

    Day30

---
+++ Day30/Project6a/Project2/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Victor Rolando Sanchez Jara on 1/23/20.
+//  Copyright © 2020 Victor Rolando Sanchez Jara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    var questionsAsked = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showScore))
+
+        setupCountries()
+        askQuestion()
+        // askQuestion(action: nil)= nil
+    }
+    
+    func setupCountries(){
+//        countries.append("estonia")
+//        countries.append("france")
+//        countries.append("germany")
+//        countries.append("ireland")
+//        countries.append("italy")
+//        countries.append("monaco")
+//        countries.append("nigeria")
+//        countries.append("poland")
+//        countries.append("russia")
+//        countries.append("spain")
+//        countries.append("uk")
+//        countries.append("us")
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = "Guess: " + countries[correctAnswer].uppercased() + " ~ Current Score: \(score)"
+        
+        questionsAsked += 1
+
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Wrong! That's the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+        }
+        
+        if questionsAsked == 10 {
+            let ac = UIAlertController(title: title, message: "Final score is \(score).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Restart", style: .default, handler: restart))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }
+        
+    }
+    
+    func restart(action: UIAlertAction! = nil) {
+        score = 0
+        questionsAsked = 0
+        
+        askQuestion()
+        
+    }
+    
+    @objc func showScore() {
+        let ac = UIAlertController(title: "Score", message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+
+}
+
