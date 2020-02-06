5859f2b71f41e149433b1e42d4d2f75d33065a3c
rolisanchez <roli.s91@gmail.com>
2020-02-06 20:24:39 +0800

    Day 33

---
+++ Day33/Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Victor Rolando Sanchez Jara on 2/6/20.
+//  Copyright © 2020 Victor Rolando Sanchez Jara. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
